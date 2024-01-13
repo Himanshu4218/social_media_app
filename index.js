b1da48d58fc68631a579e764a50390e727a5c772
Himanshu <kashyaphimanshu398@gmail.com>
2024-01-13 22:51:38 +0530

    final code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from 'path'
 import cors from "cors";
+import {fileURLToPath} from 'url';
 import { connect } from "./config/db.js";
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
@@ -23,9 +24,13 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/profile', profileRoutes);
 
-app.get('/', (req, res) =>{
-  app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.resolve(__dirname, 'frontend', 'dist')));
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
 })
 
 app.listen(5000, () => {
